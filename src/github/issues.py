38ac9d5e141056c67aee5c87f161f672a3d5872b
Kevin Knights <kevinknights29@hotmail.com>
2023-12-23 04:37:45 -0500

    feat: added error handling

--- src/github/issues.py
+++ src/github/issues.py
@@ -11,29 +11,97 @@ from src.utils import common
 # Load Env Vars
 _ = load_dotenv(find_dotenv())
 
+# Load Config
+CONFIG = common.config()
+
+# Initialize Logger
+logger = common.create_logger(__name__)
+
 # Constants
-API_BASE_URL = "https://api.github.com"
 AUTH_TOKEN = os.environ.get("GITHUB_AUTH_TOKEN")
-OWNER = common.config()["github"]["owner"]
-REPO = common.config()["github"]["repo"]
+API_URL = CONFIG["github"]["api_url"]
+OWNER = CONFIG["github"]["owner"]
+REPO = CONFIG["github"]["repo"]
+TIMEOUT = CONFIG["github"]["timeout"]
 
 
-def get_issues(owner: str = OWNER, repo: str = REPO) -> dict:
+def _check_response(response: requests.Response) -> requests.Response:
+    """
+    Check the response from a HTTP request and raise an exception if it indicates an error.
+
+    Args:
+        response (requests.Response): The response object from the HTTP request.
+
+    Raises:
+        requests.exceptions.HTTPError: If the response status code is not 200.
+        requests.exceptions.HTTPError: If the response body is empty.
+
+    Returns:
+        requests.Response: The response object from the HTTP request.
+    """
+
+    if response.status_code == 401:
+        message = f"Request failed with status code {response.status_code}, check your auth token"
+        logger.error(message)
+        raise requests.exceptions.HTTPError(message)
+        return
+
+    if response.status_code == 404:
+        message = f"Request failed with status code {response.status_code}, check your owner and repo"
+        logger.error(message)
+        raise requests.exceptions.HTTPError(message)
+        return
+
+    if response.status_code != 200:
+        message = f"Request failed with status code {response.status_code}"
+        logger.error(message)
+        raise requests.exceptions.HTTPError(message)
+        return
+
+    if not response.json():
+        message = "Request succeeded but response body is empty, no issues found"
+        logger.error(message)
+        raise requests.exceptions.HTTPError(message)
+        return
+
+    return response
+
+
+def get_issues(owner: str = OWNER, repo: str = REPO, api_url: str = API_URL) -> list[dict]:
     """
     Get the issues for a given repository.
 
     Args:
         owner (str, optional): The owner of the repository. Defaults to OWNER.
         repo (str, optional): The name of the repository. Defaults to REPO.
+        api_url (str, optional): The base URL for the GitHub API. Defaults to API_BASE_URL.
+            NOTE: Do not include a trailing slash.
 
     Returns:
-        dict: A dictionary containing the issues for the repository.
+        list[dict]: A list of issues for the given repository.
+
+    Raises:
+        ValueError: If the API_BASE_URL is invalid.
+        ConnectionError: If the API_BASE_URL is unreachable.
     """
-    request = requests.get(
-        url=f"{API_BASE_URL}/repos/{owner}/{repo}/issues",
-        headers={
-            "Authorization": f"Bearer {AUTH_TOKEN}",
-            "Accept": "application/vnd.github+json",
-        },
-    )
-    return request.json()
+    try:
+        request = requests.get(
+            url=f"{API_URL}/repos/{owner}/{repo}/issues",
+            headers={
+                "Authorization": f"Bearer {AUTH_TOKEN}",
+                "Accept": "application/vnd.github+json",
+            },
+            timeout=TIMEOUT,
+        )
+        request = _check_response(request)
+        return request.json()
+    except requests.exceptions.MissingSchema as e:
+        message = f"Invalid URL: {API_URL}"
+        logger.error(message)
+        raise ValueError(message) from e
+        return
+    except requests.exceptions.ConnectionError as e:
+        message = f"Unable to connect to {API_URL}"
+        logger.error(message)
+        raise ConnectionError(message) from e
+        return
