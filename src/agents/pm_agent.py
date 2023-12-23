cda318f4d01b72d1247c56b7ea07fd302918560b
Kevin Knights <kevinknights29@hotmail.com>
2023-12-23 12:48:44 -0500

    fix: updated PM agent with Agent class

--- src/agents/pm_agent.py
+++ src/agents/pm_agent.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from dotenv import find_dotenv
 from dotenv import load_dotenv
 from langchain.agents import AgentType
-from langchain.agents import initialize_agent
 from langchain.tools import StructuredTool
 from langchain_community.chat_models import ChatOpenAI
 
+from src.agents import agent
 from src.github import issues
 from src.utils import common
 
@@ -22,9 +22,12 @@ logger = common.create_logger(__name__)
 # Constants
 MODEL = CONFIG["openai"]["model"]
 TEMPERATURE = CONFIG["openai"]["temperature"]
-
-
-llm = ChatOpenAI(model_name=MODEL, temperature=TEMPERATURE)
+PM_PROMPT = (
+    "Act as Project Manager, break down the following task and assign role "
+    "(i.e.: Python Developer, Frontend Developer, etc.) to each subtask. "
+    "Comment the result on GitHub."
+)
+PM_AGENT_USER_INPUT_TEMPLATE = "Number: {issue_number}\n\nTitle: {issue_title}\n\nDescription: {issue_body}"
 
 
 def comment_on_github_issue(issue_number: int, comment: str) -> str:
@@ -38,21 +41,10 @@ def comment_on_github_issue(issue_number: int, comment: str) -> str:
     return "Commented on GitHub Issue"
 
 
-tool = StructuredTool.from_function(comment_on_github_issue)
-
-agent_executor = initialize_agent(
-    [tool],
-    llm,
-    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+PM_AGENT = agent.Agent(
+    llm=ChatOpenAI(model_name=MODEL, temperature=TEMPERATURE),
+    tools=[StructuredTool.from_function(comment_on_github_issue)],
+    prompt=PM_PROMPT,
+    type=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True,
 )
-
-issue = issues.get_issues()[0]
-task_prompt = """Act as Project Manager, break down the following task
-and assign role (i.e.: Python Developer, Frontend Developer, etc.) to each subtask.
-Comment the result on GitHub."""
-user_input = f"""{task_prompt} Task:```
-Number: {issue['number']}\n\nTitle: {issue['title']}\n\n Description: {issue['body']}
-```
-"""
-agent_executor.invoke({"input": f"{user_input}"})
