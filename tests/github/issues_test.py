fd6a0bedea532b41b701a0719f0379255cac5ed0
Kevin Knights <kevinknights29@hotmail.com>
2023-12-23 11:48:20 -0500

    feat: added create comment test

--- tests/github/issues_test.py
+++ tests/github/issues_test.py
@@ -7,8 +7,12 @@ from src.github import issues
 
 def main():
     # test issues
-    response = issues.get_issues()
-    print("Get Issues: ", json.dumps(response, indent=" " * 4), sep="\n")
+    issues_response = issues.get_issues()
+    print("Get Issues: ", json.dumps(issues_response, indent=" " * 4), sep="\n")
+    issue = issues_response[0]
+    issue_number = issue["number"]
+    comment_response = issues.create_comment_on_issue(issue_number=issue_number, comment="Test Comment")
+    print("Create Comment on Issue: ", json.dumps(comment_response, indent=" " * 4), sep="\n")
 
 
 if __name__ == "__main__":
