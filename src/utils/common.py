ffa511f41cc84b90cbdf8166509264195e2c8029
Kevin Knights <kevinknights29@hotmail.com>
2023-12-23 04:34:41 -0500

    feat: added logging

--- src/utils/common.py
+++ src/utils/common.py
@@ -9,29 +9,49 @@ from yaml.loader import SafeLoader
 from src.utils.constants import CONFIG_FILE
 from src.utils.constants import LOGGING_FORMAT
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter(LOGGING_FORMAT)
+def create_logger(logger_name, level=logging.INFO, fmt=LOGGING_FORMAT):
+    """
+    Creates and configures a logger object.
 
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+    Args:
+        logger_name (str): The name of the logger.
+        level (int): The log level to use (default: logging.INFO).
 
-conf: dict = {}
+    Returns:
+        logging.Logger: The configured logger object.
+    """
+    if logger_name in create_logger.loggers:
+        return create_logger.loggers[logger_name]
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    formatter = logging.Formatter(fmt)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    create_logger.loggers[logger_name] = logger
+    return logger
 
 
 def config() -> dict:
     """
-    Loads the config.yaml file and returns a dict
-    stored in config
+    Loads the config.yaml file and returns a dict stored in config.
+
     Returns:
         dict: configuration dictionary
     """
-
-    global conf
-    if not conf:
+    if not hasattr(config, "_config"):
+        # Configuration has not been loaded yet, so load it
+        conf = {}
         with open(CONFIG_FILE, encoding="utf-8") as cfg:
             conf = yaml.load(cfg, Loader=SafeLoader)
-        logger.log(logging.INFO, "Config loaded")
-    return conf
+        setattr(config, "_config", conf)
+        create_logger(__name__).log(logging.INFO, "Config loaded")
+
+    return getattr(config, "_config")
+
+
+# Initialize the loggers dictionary
+create_logger.loggers = {}
