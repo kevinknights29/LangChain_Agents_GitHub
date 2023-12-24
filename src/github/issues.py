9096a37564fe0db5d4587a39f008b5e88f2ce997
Kevin Knights <kevinknights29@hotmail.com>
2023-12-24 11:51:49 -0500

    feat: added create_issue

--- src/github/issues.py
+++ src/github/issues.py
@@ -44,25 +44,21 @@ def _check_response(response: requests.Response) -> requests.Response:
         message = f"Request failed with status code {response.status_code}, check your auth token"
         logger.error(message)
         raise requests.exceptions.HTTPError(message)
-        return
 
     if response.status_code == 404:
         message = f"Request failed with status code {response.status_code}, check your owner and repo"
         logger.error(message)
         raise requests.exceptions.HTTPError(message)
-        return
 
     if response.status_code not in [200, 201]:
         message = f"Request failed with status code {response.status_code}"
         logger.error(message)
         raise requests.exceptions.HTTPError(message)
-        return
 
     if not response.json():
         message = "Request succeeded but response body is empty, no issues found"
         logger.error(message)
         raise requests.exceptions.HTTPError(message)
-        return
 
     return response
 
@@ -77,16 +73,16 @@ def get_issues(owner: str = OWNER, repo: str = REPO, api_url: str = API_URL) -> 
         api_url (str, optional): The base URL for the GitHub API. Defaults to API_BASE_URL.
             NOTE: Do not include a trailing slash.
 
-    Returns:
-        list[dict]: A list of issues for the given repository.
-
     Raises:
         ValueError: If the API_BASE_URL is invalid.
         ConnectionError: If the API_BASE_URL is unreachable.
+
+    Returns:
+        list[dict]: A list of issues for the given repository.
     """
     try:
         request = requests.get(
-            url=f"{API_URL}/repos/{owner}/{repo}/issues",
+            url=f"{api_url}/repos/{owner}/{repo}/issues",
             headers={
                 "Authorization": f"Bearer {AUTH_TOKEN}",
                 "Accept": "application/vnd.github+json",
@@ -96,15 +92,13 @@ def get_issues(owner: str = OWNER, repo: str = REPO, api_url: str = API_URL) -> 
         request = _check_response(request)
         return request.json()
     except requests.exceptions.MissingSchema as e:
-        message = f"Invalid URL: {API_URL}"
+        message = f"Invalid URL: {api_url}"
         logger.error(message)
         raise ValueError(message) from e
-        return
     except requests.exceptions.ConnectionError as e:
-        message = f"Unable to connect to {API_URL}"
+        message = f"Unable to connect to {api_url}"
         logger.error(message)
         raise ConnectionError(message) from e
-        return
 
 
 def create_comment_on_issue(
@@ -125,28 +119,79 @@ def create_comment_on_issue(
         api_url (str, optional): The base URL for the GitHub API. Defaults to API_BASE_URL.
             NOTE: Do not include a trailing slash.
 
-    Returns:
-        dict: The response from the GitHub API.
-
     Raises:
         ValueError: If the API_BASE_URL is invalid.
         ConnectionError: If the API_BASE_URL is unreachable.
+
+    Returns:
+        dict: The response from the GitHub API.
     """
 
     try:
-        url = f"{API_URL}/repos/{owner}/{repo}/issues/{issue_number}/comments"
+        url = f"{api_url}/repos/{owner}/{repo}/issues/{issue_number}/comments"
         headers = {"Authorization": f"Bearer {AUTH_TOKEN}", "Accept": "application/vnd.github+json"}
         data = {"body": comment}
         response = requests.post(url=url, headers=headers, json=data, timeout=TIMEOUT)
         _check_response(response)
         return response.json()
     except requests.exceptions.MissingSchema as e:
-        message = f"Invalid URL: {API_URL}"
+        message = f"Invalid URL: {api_url}"
         logger.error(message)
         raise ValueError(message) from e
-        return
     except requests.exceptions.ConnectionError as e:
-        message = f"Unable to connect to {API_URL}"
+        message = f"Unable to connect to {api_url}"
         logger.error(message)
         raise ConnectionError(message) from e
-        return
+
+
+def create_issue(
+    title: str,
+    body: str,
+    assignees: list[str] = None,
+    labels: list[str] = None,
+    owner: str = OWNER,
+    repo: str = REPO,
+    api_url: str = API_URL,
+) -> dict:
+    """_summary_
+
+    Args:
+        title (str): The title of the issue
+        body (str): The body of the issue
+        assignees (list[str], optional): The list of assignees of the issue. Defaults to [""].
+        labels (list[str], optional): The list of labels of the issue. Defaults to [""].
+        owner (str, optional): The owner of the repository. Defaults to OWNER.
+        repo (str, optional): The name of the repository. Defaults to REPO.
+        api_url (str, optional): The base URL for the GitHub API. Defaults to API_BASE_URL.
+            NOTE: Do not include a trailing slash.
+
+    Raises:
+        ValueError: If the API_BASE_URL is invalid.
+        ConnectionError: If the API_BASE_URL is unreachable.
+
+    Returns:
+        dict: The response from the GitHub API.
+    """
+    try:
+        url = f"{api_url}/repos/{owner}/{repo}/issues"
+        headers = {
+            "Authorization": f"Bearer {AUTH_TOKEN}",
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+        data = {"title": title, "body": body}
+        if assignees is not None:
+            data["assignees"] = assignees
+        if labels is not None:
+            data["labels"] = labels
+        response = requests.post(url=url, headers=headers, json=data, timeout=TIMEOUT)
+        _check_response(response)
+        return response.json()
+    except requests.exceptions.MissingSchema as e:
+        message = f"Invalid URL: {api_url}"
+        logger.error(message)
+        raise ValueError(message) from e
+    except requests.exceptions.ConnectionError as e:
+        message = f"Unable to connect to {api_url}"
+        logger.error(message)
+        raise ConnectionError(message) from e
